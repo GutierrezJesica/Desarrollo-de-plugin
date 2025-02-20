79cba31e0aaa7afc42f999a30b51dbc6daf53080
Gutierrez Jesica <jesica89@gmail.com>
2025-02-20 10:13:52 -0300

    shortcode cargado

--- shortcodes/reserva-shortcode.php
+++ shortcodes/reserva-shortcode.php
@@ -1,1 +1,19 @@
+<?php
+
 // Shortcode para mostrar el formulario
+// El usuario podrá reservar viajes desde Elementor.
+function mostrar_formulario_reserva() {
+    ob_start();
+    ?>
+    <form id="form-reserva" method="post">
+        <input type="text" name="nombre" placeholder="Tu Nombre" required>
+        <input type="email" name="email" placeholder="Tu Correo" required>
+        <input type="datetime-local" name="fecha" required>
+        <input type="text" name="destino" placeholder="Destino" required>
+        <button type="submit">Reservar</button>
+    </form>
+    <?php
+    return ob_get_clean();
+}
+add_shortcode('reserva_viaje', 'mostrar_formulario_reserva');
+?>
